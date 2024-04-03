a074ce468a7a23c7dc54ad1b78e8c0251a07090e
= <wagnersmith123@hotmail.com>
2024-04-03 12:17:37 -0500

    resolviendo conflictos

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,13 @@
 "use client";
-import ButtonAuth from "@/components/ButtonAuth";
+
+import ButtonAuth from "./components/ButtonAuth";
+
 
 export default function Home() {
   return (
      <>
       <h1>Home</h1>
       <ButtonAuth></ButtonAuth>
-    </>
+    </>)
+    }
 
