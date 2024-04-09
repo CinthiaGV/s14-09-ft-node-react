dff760cdd676abcb7bc28844b40892dfa33a3ca4
= <wagnersmith123@hotmail.com>
2024-04-09 17:36:22 -0500

    poniendole glassmorfirmo a el header

--- frontend/app/components/header/Header.tsx
+++ frontend/app/components/header/Header.tsx
@@ -6,7 +6,7 @@ import Navbar from './Navbar';
 
 export default function Header() {
   return (
-    <header className=" fixed w-full z-20 top-0 start-0 backdrop-filter backdrop-blur-sm bg-opacitclassNamey-10 backdrop-filter backdrop-blur-sm bg-opacity-0">
+    <header className="fixed w-full z-20 top-0 start-0 backdrop-filter backdrop-blur-sm bg-opacitclassNamey-10 backdrop-filter backdrop-blur-sm bg-opacity-40">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <Logo />
         <div className="flex gap-4 md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
