4ff828fc48916993756094d5ef3161af4b4101fe
= <wagnersmith123@hotmail.com>
2024-04-12 11:50:28 -0500

    funciona la validacion

--- frontend/app/register/page.tsx
+++ frontend/app/register/page.tsx
@@ -1,11 +1,9 @@
 import Register from "../components/auth/register/Register";
-import RegisterD from "../components/auth/register/RegisterD";
 
 export default function Page() {
   return (
     <div>
       <Register/>
-      <RegisterD/>
     </div>
   );
 }
