13a1d13181c9de7089ea4f3f0f77ad8947f0459e
= <wagnersmith123@hotmail.com>
2024-04-13 12:39:52 -0500

    avanzando

--- frontend/app/components/auth/register/Register.tsx
+++ frontend/app/components/auth/register/Register.tsx
@@ -1,4 +1,6 @@
-'use client'
+'use client';
+import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
+import GoogleIcon from '@mui/icons-material/Google';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import InputField from './InputField';
@@ -29,16 +31,48 @@ export default function Register() {
   });
 
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center h-screen w-[628px]">
       <div className="flex flex-col justify-center items-center text-white">
         <h2 className="text-2xl font-bold">Registrarme</h2>
-        <hr className="w-full border-b-1 overflow-visible"/>
+        <hr className="w-full border-b-1 overflow-visible" />
         <form onSubmit={formik.handleSubmit} className="flex flex-col gap-4">
-          <InputField type="text" label="Username:" id="username" formikProps={{ formik }} />
-          <InputField type="email" label="Email:" id="email" formikProps={{ formik }} />
-          <InputField type="password" label="Password:" id="password" formikProps={{ formik }} />
-          <button className="yellowBtn glitch transition duration-300" type="submit">Registrarme</button>
+          <InputField
+            type="text"
+            label="Username:"
+            id="username"
+            formikProps={{ formik }}
+          />
+          <InputField
+            type="email"
+            label="Email:"
+            id="email"
+            formikProps={{ formik }}
+          />
+          <InputField
+            type="password"
+            label="Password:"
+            id="password"
+            formikProps={{ formik }}
+          />
+          <div>
+            <a><span>¿Olvidaste tu contraseña?</span></a>
+          </div>
+          <button
+            className="yellowBtn glitch transition duration-300"
+            type="submit"
+          >
+            Registrarme
+          </button>
         </form>
+        <div className="separator">
+          <div></div>
+          <span>O regístrate con</span>
+          <div></div>
+        </div>
+        <div className="inline">
+        <button><GoogleIcon/></button>
+        <button><FacebookRoundedIcon/></button>
+        </div>
       </div>
     </div>
   );
