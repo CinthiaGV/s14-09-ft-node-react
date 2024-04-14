c92a95df8f535365b1412b176d59f303ba10c6af
Matias <matias.semelman@protonmail.com>
2024-04-14 19:15:10 -0300

    Refactor GameSelector component to handle selected games

--- frontend/app/components/Profile/GameSelector/index.tsx
+++ frontend/app/components/Profile/GameSelector/index.tsx
@@ -1,39 +1,46 @@
+import { useState } from 'react';
+
 const games = [
-  {
-    id: 1,
-    name: 'Super Mario Bros.',
-    image:
-      'https://th.bing.com/th/id/R.d588a6537b3ac4d3ae5eed78f9db714f?rik=%2bqZKFuEGOWG8bg&riu=http%3a%2f%2fwallup.net%2fwp-content%2fuploads%2f2016%2f07%2f19%2f11992-Super_Mario-Mario_Bros..jpg&ehk=CBpg7GmHDAt3KVRIsM4hVI9EdoPNQqwzz44Ft5TFnhs%3d&risl=&pid=ImgRaw&r=0',
-  },
-  {
-    id: 2,
-    name: 'The Legend of Zelda',
-    image:
-      'https://th.bing.com/th/id/OIP.Wm9-vU0Z16ymtdEcZCB2rwHaKJ?rs=1&pid=ImgDetMain',
-  },
+  'Simulation',
+  'Action',
+  'Adventure',
+  'Strategy',
+  'Role-Playing',
+  'Sports',
 ];
 
-// export const GameSelector = ({ games, onSelectGame }) => {
-export const GameSelector = ({ onSelectGame }) => {
+interface GameSelectorProps {
+  onSelectGame: (selectedGames: string[]) => void;
+  selectedGames: string[];
+}
+
+export const GameSelector = ({
+  onSelectGame,
+  selectedGames,
+}: GameSelectorProps) => {
+  const handleGameChange = (game: string) => {
+    const updatedSelectedGames = selectedGames.includes(game)
+      ? selectedGames.filter((g) => g !== game)
+      : [...selectedGames, game];
+
+    onSelectGame(updatedSelectedGames);
+  };
   return (
-    <div className="grid grid-cols-3 gap-4">
+    <div className="flex flex-wrap">
       {games.map((game) => (
-        <div
-          key={game.id}
-          className="relative cursor-pointer"
-          onClick={() => onSelectGame(game)}
+        <label
+          key={game}
+          className="flex items-center m-2 bg-white text-black px-4 py-2 border border-black rounded-3xl cursor-pointer"
         >
-          <div className="w-32 h-32 overflow-hidden rounded">
-            <img
-              src={game.image}
-              alt={game.name}
-              className="w-full h-full object-fill"
-            />
-          </div>
-          <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 opacity-0 hover:opacity-100 transition duration-200">
-            <span className="text-white font-semibold">{game.name}</span>
-          </div>
-        </div>
+          <input
+            type="checkbox"
+            className="mr-2 hidden"
+            value={game}
+            checked={selectedGames.includes(game)}
+            onChange={() => handleGameChange(game)}
+          />
+          {game}
+        </label>
       ))}
     </div>
   );
