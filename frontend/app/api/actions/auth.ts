352dde49daf2f5d7001861bd41ebc09d5c2b4750
= <wagnersmith123@hotmail.com>
2024-04-14 15:18:38 -0500

    me funciona el login

--- frontend/app/api/actions/auth.ts
+++ frontend/app/api/actions/auth.ts
@@ -14,12 +14,15 @@ export const registerUser = async (values: IUser) => {
   }
 };
 
+export let validToken:Boolean = false
+
 export const LoginUser = async (values: ILogIn) => {
   try {
     console.log("values", values)
     const info:ILogIn = values
     const { data } = await axios.post(`${endpoint}/users/signin`, info);
     console.log('loginData', data)
+    if(data.meta.token) validToken = true
     return data
   } catch (error) {
     console.log('Unsucessful login!', error);
