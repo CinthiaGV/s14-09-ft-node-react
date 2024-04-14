6dad06c56d3befaf545fcb664b2db6ad5118e003
Matias <matias.semelman@protonmail.com>
2024-04-14 19:15:19 -0300

    Fix typos and update class names in frontend files

--- frontend/app/components/Profile/ProfileForm.tsx
+++ frontend/app/components/Profile/ProfileForm.tsx
@@ -5,30 +5,19 @@ import Image from 'next/image';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import './styles.css';
-import {
-  CellphoneIcon,
-  ComputerIcon,
-  ConsoleIcon,
-  PlusIcon,
-} from '../../assets';
-import addGameImage from '../../assets/addGame.png';
-import {
-  Popover,
-  PopoverTrigger,
-  PopoverContent,
-  Button,
-} from '@nextui-org/react';
+import { CellphoneIcon, ComputerIcon, ConsoleIcon } from '../../assets';
+
 import { GameSelector } from './GameSelector';
 
 const ProfileForm = () => {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null);
   const [profilePic, setProfilePic] = useState<string | null>(null);
   const [selectedPlatform, setSelectedPlatform] = useState('mobile');
-  const [selectedGames, setSelectedGames] = useState([]);
+  const [selectedGames, setSelectedGames] = useState<string[]>([]);
 
   useEffect(() => {
-    console.log(selectedPlatform);
-  }, [selectedPlatform]);
+    console.log(selectedGames);
+  }, [selectedGames]);
 
   const {
     register,
@@ -46,19 +35,19 @@ const ProfileForm = () => {
   };
 
   return (
-    <div className="pl-[82px] py-[104px] text-[#FFFFFF]">
+    <div className="px-4 sm:pl-[82px] py-[104px] text-[#FFFFFF]">
       {' '}
       <h4 className=" pb-[41px] text-4xl">Termina de completar tu Perfil</h4>
-      <form onSubmit={handleSubmit(onSubmit)} className="space-y-6 w-[50%] ">
-        <div className="border-2 mb-10 border-gray-200 rounded-lg w-[80%]">
-          <div className="px-6 py-2">
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-6 sm:w-[50%] ">
+        <div className="border-2 mb-2 sm:mb-10  border-gray-200 rounded-lg w-[80%]">
+          <div className="px-6 py-1">
             <label
               htmlFor="username"
               className="block sm:text-md text-gray-400 opacity-90 "
             >
               Nombre completo
             </label>
-            <div className="mt-1">
+            <div>
               <input
                 type="text"
                 id="username"
@@ -72,13 +61,14 @@ const ProfileForm = () => {
           </div>
         </div>
 
-        <div className="flex gap-2   ">
-          <div className="w-[60%] border-gray-300 border-2 rounded-md">
+        <div className="flex flex-col sm:flex-row gap-2   ">
+          <div className="w-[60%] h-12 border-gray-300 border-2 rounded-md">
             <div className="mt-1 px-2">
               <DatePicker
-                className=" bg-transparent pl-2 text-md focus:outline-none "
+                className=" bg-transparent pl-2 pt-2 text-md focus:outline-none "
                 selected={selectedDate}
                 onChange={(date) => setSelectedDate(date)}
+                placeholderText="MM/DD/YYYY"
               />
             </div>
           </div>
@@ -103,11 +93,10 @@ const ProfileForm = () => {
             </div>
           </div>
         </div>
-        <div className="!mt-0 pl-4">MM/DD/YYYY</div>
 
         <div>
-          <h5 className="pb-2 text-xl mt-10">Plataforma preferida</h5>
-          <div className="flex gap-10 ">
+          <h5 className="pb-2 text-xl sm:mt-10 mt-2">Plataforma preferida</h5>
+          <div className="flex gap-10 mt-10 ">
             <label
               className={`w-fit flex flex-col items-center justify-center text-sm ${
                 selectedPlatform === 'mobile' &&
@@ -168,100 +157,12 @@ const ProfileForm = () => {
         <div>
           <h4>Agrega los juegos que te gustan</h4>
           <div className="flex gap-1">
-            <Popover placement="bottom" offset={0}>
-              <PopoverTrigger>
-                <Button>
-                  <Image
-                    src={addGameImage}
-                    alt="Add Game"
-                    width={90}
-                    height={75}
-                  />
-                  <PlusIcon classNames="absolute translate-y-[-50%]" />
-                </Button>
-              </PopoverTrigger>
-              <PopoverContent>
-                <div className="px-1 py-2">
-                  <GameSelector onSelectGame={setSelectedGames} />
-                </div>
-              </PopoverContent>
-            </Popover>
-            <Popover placement="bottom" offset={0}>
-              <PopoverTrigger>
-                <Button>
-                  <Image
-                    src={addGameImage}
-                    alt="Add Game"
-                    width={90}
-                    height={75}
-                  />
-                  <PlusIcon classNames="absolute translate-y-[-50%]" />
-                </Button>
-              </PopoverTrigger>
-              <PopoverContent>
-                <div className="px-1 py-2">
-                  <GameSelector onSelectGame={setSelectedGames} />
-                </div>
-              </PopoverContent>
-            </Popover>
-            <Popover placement="bottom" offset={0}>
-              <PopoverTrigger>
-                <Button>
-                  <Image
-                    src={addGameImage}
-                    alt="Add Game"
-                    width={90}
-                    height={75}
-                  />
-                  <PlusIcon classNames="absolute translate-y-[-50%]" />
-                </Button>
-              </PopoverTrigger>
-              <PopoverContent>
-                <div className="px-1 py-2">
-                  <GameSelector onSelectGame={setSelectedGames} />
-                </div>
-              </PopoverContent>
-            </Popover>
-          </div>
-        </div>
-
-        {/* <div>
-          <label htmlFor="profilePic" className="block text-sm font-medium ">
-            Foto de perfil
-          </label>
-          <div className="mt-1 flex items-center">
-            <input
-              type="file"
-              id="profilePic"
-              accept="image/*"
-              onChange={handleProfilePicChange}
-              className="block w-full text-sm  file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-indigo-50 file:text-indigo-700 hover:file:bg-indigo-100"
-            />
-            {profilePic && (
-              <Image
-                src={profilePic}
-                alt="Profile"
-                className="ml-4 h-16 w-16 rounded-full"
-                width={64}
-                height={64}
-              />
-            )}
-          </div>
-        </div>
-
-        <div>
-          <label htmlFor="description" className="block text-sm font-medium ">
-            Breve descripción personal
-          </label>
-          <div className="mt-1">
-            <textarea
-              id="description"
-              rows={3}
-              {...register("description")}
-              className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+            <GameSelector
+              onSelectGame={setSelectedGames}
+              selectedGames={selectedGames}
             />
           </div>
-        </div> */}
+        </div>
 
         <div>
           <button
