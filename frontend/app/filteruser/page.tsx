6d94f55754b4647d149deadfee4139b214a350e4
Matias Campos <lolo252000@gmail.com>
2024-04-17 00:24:39 -0300

    feat: changing card

--- frontend/app/filteruser/page.tsx
+++ frontend/app/filteruser/page.tsx
@@ -2,34 +2,99 @@
 import { FaMobileAlt } from 'react-icons/fa';
 import { RiComputerLine } from 'react-icons/ri';
 import { GiConsoleController } from 'react-icons/gi';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const FilterUser = () => {
   const [inputValue, setInputValue] = useState('');
-  const [username, setUsername] = useState('');
-  const [age, setAge] = useState('');
-  const [bio, Setbio] = useState('');
-  const [nameGame, setNameGame] = useState('');
-  const [categoryGame, setCategoryGame] = useState('');
-  const [skillLevel, setSkillLevel] = useState('');
-  const [favoritePlatform, setFavoritePlatform] = useState('');
+  const [tarjetas, setTarjetas] = useState([]);
+  const [indice, setIndice] = useState(0);
+  // const [username, setUsername] = useState('');
+  // const [age, setAge] = useState('');
+  // const [bio, Setbio] = useState('');
+  // const [image, setImage] = useState('');
+  // const [nameGame, setNameGame] = useState('');
+  // const [categoryGame, setCategoryGame] = useState('');
+  // const [skillLevel, setSkillLevel] = useState('');
+  // const [favoritePlatform, setFavoritePlatform] = useState('');
+
+  // useEffect(() => {
+  //   const fetchCards = async () => {
+  //     const res = await fetch(
+  //       `${process.env.NEXT_PUBLIC_BACKEND_URL}/users/signin`,
+  //       {
+  //         method: 'GET',
+  //         headers: {
+  //           'Content-Type': 'application/json',
+  //           authorization: `Bearer ${session?.user?.token}`,
+  //         },
+  //       }
+  //     );
+  //     const cards = await res.json();
+  //     console.log('router user', cards);
+  //     setTarjetas(cards);
+  //     if (cards.error) {
+  //       throw new Error(cards.error.message);
+  //     }
+  //     return cards;
+  //   };
+  // });
+  useEffect(() => {
+    //simulando un fetch
+    const datosDePrueba = [
+      { username: 'Perro', age: 25, bio: 'Descripción del usuario 1' },
+      { username: 'Gato', age: 30, bio: 'dfhjytjyuhkuykuikuykuyku' },
+      { username: 'Tortuga', age: 35, bio: 'AAAAAAAAAAAAAA' },
+    ];
+
+    setTarjetas(datosDePrueba);
+  }, []);
+
+  const tarjetaSeleccionada = tarjetas[indice];
+  const tarjetaAnterior = () => {
+    if (indice > 0) {
+      setIndice(indice - 1);
+    }
+  };
+
+  const tarjetaPosterior = () => {
+    if (indice < tarjetas.length - 1) {
+      setIndice(indice + 1);
+    }
+  };
 
   const handleButtonClick = () => {};
+
   return (
     <>
-      <div className="h-screen w-screen bg-black flex">
+      <div className="h-screen w-screen bg-[#232222] flex">
         <div className="w-2/3 h-fullflex items-center justify-center">
-          <div className="w-full h-full flex flex-col pt-32 pb-8 pr-16 pl-32">
-            <div className="flex-1 bg-[url('/assets/avatar.jpg')] bg-cover mr-20 ml-20 max-w[300px]">
+          <div className="relative w-full h-full flex flex-col pt-32 pb-8 pr-16 pl-32">
+            <div className="absolute bottom-80 text-2xl">
+              <button
+                className="bg-[#36343b] h-16 w-16"
+                onClick={tarjetaAnterior}
+              >
+                &lt;
+              </button>
+              <button
+                className="bg-[#36343b] h-16 w-16 absolute left-[480px]"
+                onClick={tarjetaPosterior}
+              >
+                &gt;
+              </button>
+            </div>
+            <div className="flex-1 bg-[url('/assets/avatar.jpg')] bg-cover mr-20 ml-20 max-w-sm">
               <div className="flex flex-col text-white h-full justify-end">
-                <div className="flex">
-                  <p className="">Perrito</p>
-                  <p className="">18 años</p>
+                <div className="backdrop-filter backdrop-blur-sm backdrop-saturate-100 bg-opacity-50 bg-opacity-50">
+                  <div className="flex gap-3">
+                    <p className="">{tarjetaSeleccionada?.username}</p>
+                    <p className="">{tarjetaSeleccionada?.age}</p>
+                  </div>
+                  <p className="">{tarjetaSeleccionada?.bio}</p>
                 </div>
-                <p className="">Descripcion</p>
               </div>
             </div>
-            <div className="flex-1 mr-20 ml-20 h-full flex flex-col justify-around max-w[300px]">
+            <div className="flex-1 mr-20 ml-20 h-full flex flex-col justify-around max-w-sm bg-[#201e1e]">
               <div className="flex flex-col gap-2">
                 <h3 className="">Juegos</h3>
                 <div className="flex gap-x-3">
@@ -126,7 +191,7 @@ const FilterUser = () => {
               </div>
               <div className="flex flex-col gap-2">
                 <h3 className="pl-14">Enviale una solicitud de mensaje!</h3>
-                <div className="relative flex">
+                <div className="relative flex justify-end">
                   <div className="w-5/6 max-w-[350px]">
                     <input
                       type="text"
