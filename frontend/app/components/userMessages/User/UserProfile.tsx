111b3560a76ab13ed37f82bc7dc052bc9a8bba3b
DavidMoralesz4 <jd3285201@gmail.com>
2024-04-18 15:00:35 -0500

    profile-center

--- frontend/app/components/userMessages/User/UserProfile.tsx
+++ frontend/app/components/userMessages/User/UserProfile.tsx
@@ -1,12 +1,14 @@
 import { IUserProfile } from '../interfaces';
 import NoProfilePhoto from './NoProfilePhoto';
 
-export default function UserProfile({ user }: IUserProfile ) {
+export default function UserProfile({ user }: IUserProfile) {
   return (
-    <div className=''>
-      {!user.profile_picture
-      ?<NoProfilePhoto name={user.name}/>
-    :<>fgdfgh</>}
+    <div className="">
+      {!user.profile_picture ? (
+        <NoProfilePhoto name={user.name} />
+      ) : (
+        <>fgdfgh</>
+      )}
     </div>
   );
 }
