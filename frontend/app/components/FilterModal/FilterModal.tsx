dedad66b858971cb94e4f0f75249ca1b2eb4536f
Javier Damiani <javierdamiani74@gmail.com>
2024-04-17 20:27:59 -0500

    falta darle estilos al modal

--- frontend/app/components/FilterModal/FilterModal.tsx
+++ frontend/app/components/FilterModal/FilterModal.tsx
@@ -1,23 +1,56 @@
 import React from 'react';
 import './FilterModal.Module.css';
 
-function FilterModal({ isOpen, setIsOpen, toggleModal }) {
-  const closeModal = () => {
-    setIsOpen(false);
-  };
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const FilterModal: React.FC<ModalProps> = ({ isOpen, onClose }) => {
+  if (!isOpen) return null;
+
   return (
-    <>
-      {isOpen && (
-        <div className="overlay" onClick={closeModal}>
-          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-            <form>
-              <p>Soy un modal</p>
-            </form>
+    <div className="modal-overlay">
+      <div className="modal-content">
+        <button className="close-btn" onClick={onClose}>
+          X
+        </button>
+        <hr className="divider" />
+        <div className="modal-columns">
+          <div className="modal-column">
+            <h2>Tipos de juego</h2>
+            <label>
+              <input type="checkbox" /> Acción
+            </label>
+            <label>
+              <input type="checkbox" /> Aventura
+            </label>
+            <label>
+              <input type="checkbox" /> Deportes
+            </label>
+            <label>
+              <input type="checkbox" /> Estrategia
+            </label>
+            <label>
+              <input type="checkbox" /> Simulación
+            </label>
+          </div>
+          <div className="modal-column">
+            <h2>Nivel de habilidad</h2>
+            <label>
+              <input type="checkbox" /> 1 estrella
+            </label>
+            <label>
+              <input type="checkbox" /> 2 estrellas
+            </label>
+            <label>
+              <input type="checkbox" /> 3 estrellas
+            </label>
           </div>
         </div>
-      )}
-    </>
+      </div>
+    </div>
   );
-}
+};
 
 export { FilterModal };
