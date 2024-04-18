df0cd9b6a9fa9cb7f34c36d5eee1fce95f46e18c
= <wagnersmith123@hotmail.com>
2024-04-18 13:36:34 -0500

    online funcionando y el scroll tambien

--- frontend/app/components/userMessages/UserMessages.tsx
+++ frontend/app/components/userMessages/UserMessages.tsx
@@ -7,12 +7,14 @@ import { IUserMessages } from "./interfaces";
 
 export default function UserMessages({ user, messages }:IUserMessages) {
   return (
-    <div className='w-1/3 grid grid-row-3 wrapper'>
-      <div className='row-span-1 border-b-1 border-white justify-center'>
+    <div className='w-1/3 grid grid-row-3'>
+      <div className='row-span-1 border-b-1 border-white'>
       <UserProfile user={user}/>
       </div>
+      <div className="hover:overflow-auto w-full h-auto">
       <div className='row-start-2'>
       <Contacts messages={messages}/>
+      </div>
       </div>
     </div>
   );
