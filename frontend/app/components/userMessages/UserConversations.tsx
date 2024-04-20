6c62df6fad3995e02cd3a3d488063d7ad3647a61
= <wagnersmith123@hotmail.com>
2024-04-20 14:13:21 -0500

    avanzando

--- frontend/app/components/userMessages/UserConversations.tsx
+++ frontend/app/components/userMessages/UserConversations.tsx
@@ -9,7 +9,7 @@ import { IUserConversations } from './interfaces';
 
 export default function UserConversations({ user, conversations }: IUserConversations):ReactNode {
   return (
-    <div className="w-1/3 grid grid-row-3">
+    <div className="w-1/3 grid grid-rows-3">
       <div className="flex justify-center items-center row-span-1 border-b-2 border-l-2 border-[#49454F] bg-[#131212] h-[300px] ">
         <div className="flex flex-col justify-center items-center">
           <UserProfile user={user} />
@@ -18,7 +18,7 @@ export default function UserConversations({ user, conversations }: IUserConversa
         </div>
       </div>
       <div className="hover:overflow-auto w-full h-auto bg-[#131212] border-l-2 border-[#49454F]">
-        <p className="text-[#ACA5A5] px-10 mt-5 text-[18px] ">Mensajes</p>
+        {/*<p className="text-[#ACA5A5] px-10 mt-5 text-[18px] ">Mensajes</p>*/}
         <div className="row-start-2">
           {/*<ConversationArea conversations={conversations} />*/}
           <ChatArea conversation={conversations[0]}/>
