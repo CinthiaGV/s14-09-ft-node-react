6ba2026a5bbe8cc0cbfbcc7fc49cce649520e933
lags2022 <lguzman.58erb@outlook.com>
2024-04-21 14:38:48 -0500

    feat: conectantdo messages

--- frontend/app/components/userMessages/Chat/ChatAreaHeader.tsx
+++ frontend/app/components/userMessages/Chat/ChatAreaHeader.tsx
@@ -1,16 +1,22 @@
 import { Image } from '@nextui-org/react';
 import { IChatArea } from '../interfaces';
 
-export default function ChatAreaHeader({ conversation }: IChatArea) {
-  const profile_picture = conversation?.recipient?.profile_picture || "";
-  const name = conversation?.recipient?.name || "";
+export default function ChatAreaHeader({
+  conversation,
+  handleShowMessage,
+}: IChatArea & { handleShowMessage: (status: boolean) => void }) {
+  const profile_picture = conversation?.recipient?.profile_picture || '';
+  const name = conversation?.recipient?.name || '';
 
   return (
-    <div className="chat-area-header">
-      <div className="chat-area-profile">
-        <Image src={profile_picture} alt={name} />
+    <>
+      <button onClick={() => handleShowMessage(true)}>Regresar</button>
+      <div className="chat-area-header">
+        <div className="chat-area-profile">
+          <Image src={profile_picture} alt={name} />
+        </div>
+        <div className="chat-area-title">{name}</div>
       </div>
-      <div className="chat-area-title">{name}</div>
-    </div>
+    </>
   );
 }
