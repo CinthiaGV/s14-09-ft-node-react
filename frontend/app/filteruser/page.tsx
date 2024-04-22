b1f76b342e3280b19558e25a56059ac38b17cf1b
Matias Campos <lolo252000@gmail.com>
2024-04-22 13:26:06 -0300

    feat: agregando el filtrado

--- frontend/app/filteruser/page.tsx
+++ frontend/app/filteruser/page.tsx
@@ -5,7 +5,13 @@ import { GiConsoleController } from 'react-icons/gi';
 import { useEffect, useState } from 'react';
 import { useSession } from 'next-auth/react';
 import { differenceInYears, parseISO } from 'date-fns';
-import { useRadio } from '@nextui-org/react';
+import './FilterModal.Module.css';
+import './FilterModule.css';
+
+interface ModalProps {
+  isOpen: boolean;
+  toggleModal: () => void;
+}
 
 const FilterUser = () => {
   const { data: session, status } = useSession();
@@ -165,6 +171,7 @@ const FilterUser = () => {
         <div className="h-screen w-screen flex">
           <div className="w-2/3 h-fullflex items-center justify-center">
             <div className="relative w-full h-full flex flex-col pt-32 pb-8 pr-16 pl-32">
+              <Filter />
               <div className="absolute bottom-80 text-2xl">
                 <button
                   className="bg-[#36343b] h-16 w-16"
@@ -379,3 +386,139 @@ const FilterUser = () => {
 };
 
 export default FilterUser;
+
+const FilterModal: React.FC<ModalProps> = ({ isOpen, toggleModal }) => {
+  const [gameType, setGameType] = useState<string>('');
+  const [skillLevel, setSkillLevel] = useState<string>('');
+
+  const handleGameTypeChange = (type: string) => {
+    setGameType(type === gameType ? '' : type);
+  };
+
+  const handleSkillLevelChange = (level: string) => {
+    setSkillLevel(level === skillLevel ? '' : level);
+  };
+
+  const handleFilterUsers = () => {
+    const filters = [];
+    if (gameType) filters.push(`gameType=${gameType}`);
+    if (skillLevel) filters.push(`skill=${skillLevel}`);
+    const queryString = filters.join('&');
+    const apiUrl = `https://api-tinder-games.onrender.com/api/v1/users/filter?${queryString}`;
+
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Filtered users:', data);
+      })
+      .catch((error) => {
+        console.error('Error fetching filtered users:', error);
+      });
+  };
+
+  if (!isOpen) return null;
+
+  return (
+    <div className="modal-overlay" onClick={toggleModal}>
+      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+        <button className="close-btn" onClick={toggleModal}>
+          X BOTON X!!!
+        </button>
+        <hr className="divider" />
+        <div className="modal-columns">
+          <div className="modal-column">
+            <h2>Tipos de juego</h2>
+            <label className="checkbox-container">
+              <input
+                type="checkbox"
+                className="checkbox-input"
+                checked={gameType === 'Action'}
+                onChange={() => handleGameTypeChange('Action')}
+              />
+              <span className="checkbox-custom"></span>
+              Action
+            </label>
+            <label className="checkbox-container">
+              <input
+                type="checkbox"
+                className="checkbox-input"
+                checked={gameType === 'Sport'}
+                onChange={() => handleGameTypeChange('Sport')}
+              />
+              <span className="checkbox-custom"></span>
+              Sport
+            </label>
+            <label className="checkbox-container">
+              <input
+                type="checkbox"
+                className="checkbox-input"
+                checked={gameType === 'Strategy'}
+                onChange={() => handleGameTypeChange('Strategy')}
+              />
+              <span className="checkbox-custom"></span>
+              Strategy
+            </label>
+            <label className="checkbox-container">
+              <input
+                type="checkbox"
+                className="checkbox-input"
+                checked={gameType === 'Simulation'}
+                onChange={() => handleGameTypeChange('Simulation')}
+              />
+              <span className="checkbox-custom"></span>
+              Simulation
+            </label>
+          </div>
+          <div className="modal-column">
+            <h2>Nivel de habilidad</h2>
+            <label className="checkbox-container">
+              <input
+                type="checkbox"
+                className="checkbox-input"
+                checked={skillLevel === '1'}
+                onChange={() => handleSkillLevelChange('1')}
+              />
+              <span className="checkbox-custom"></span>1 estrella
+            </label>
+            <label className="checkbox-container">
+              <input
+                type="checkbox"
+                className="checkbox-input"
+                checked={skillLevel === '2'}
+                onChange={() => handleSkillLevelChange('2')}
+              />
+              <span className="checkbox-custom"></span>2 estrellas
+            </label>
+            <label className="checkbox-container">
+              <input
+                type="checkbox"
+                className="checkbox-input"
+                checked={skillLevel === '3'}
+                onChange={() => handleSkillLevelChange('3')}
+              />
+              <span className="checkbox-custom"></span>3 estrellas
+            </label>
+          </div>
+        </div>
+        <button onClick={handleFilterUsers}>Filtrar Usuarios</button>
+      </div>
+    </div>
+  );
+};
+
+function Filter() {
+  return (
+    <>
+      <div className="skewed-border" onClick={toggleModal}>
+        <p className="p-3">Filtrar por</p>
+        <Image
+          src="/assets/menu.svg"
+          alt="Esto es un menu"
+          width={24}
+          height={24}
+        />
+      </div>
+      <FilterModal />
+    </>
+  );
+}
